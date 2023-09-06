b7f1dbed3202c643fee0dfaac7068987268a27e5
Gustavo Henrique <gugu2305@hotmail.com>
2023-09-06 15:44:44 -0300

    Exercicio Finalizado

---
+++ Exercicio/Entities/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Entities {
+    internal class Funcionario {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string nome, string email, double salario) {
+            Nome = nome;
+            Email = email;
+            Salario = salario;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", "
+                + Email 
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.IO;
+using Entities;
+
+
+internal class Program {
+    private static void Main(string[] args) {
+
+        Console.Write("Entre com o caminho do arquivo: ");
+        string path = Console.ReadLine();
+
+        Console.Write("Entre com salario: ");
+        double sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        try {
+            List<Funcionario> func = new List<Funcionario>();
+
+            using (StreamReader sr = File.OpenText(path)) {
+                while (!sr.EndOfStream) {
+
+                    string[] vet = sr.ReadLine().Split(',');
+
+                    string name = vet[0];
+                    string mail = vet[1];
+                    double salario = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+                    func.Add(new Funcionario(name, mail, salario));
+                }
+
+                Console.WriteLine();
+                foreach (Funcionario fun in func) {
+                    Console.WriteLine(fun);
+                }
+                Console.WriteLine();
+
+                var email = func.Where(f => f.Salario > sal).OrderBy(f => f.Email).Select(f => f.Email);
+                foreach (string em in email) {
+                    Console.WriteLine(em);
+                }
+
+                Console.WriteLine();
+                var soma = func.Where(f => f.Nome[0] == 'M').Sum(f => f.Salario);
+                Console.Write("Soma dos salarios das pessoas que começam com M: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+        catch (IOException e) {
+            Console.WriteLine("ERRO");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
